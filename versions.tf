6c750b60aa0f148d5986626369e39418cb879b3c
zhaojiaqiang <zhaojiaqiang@idcos.com>
2022-06-22 17:54:03 +0800

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,11 +1,11 @@
 terraform {
   required_providers {
     ansible = {
-      source = "registry.cloudiac.org/nbering/ansible"
+      source = "nbering/ansible"
       version = "1.0.4"
     }
     vsphere = {
-      source  = "registry.cloudiac.org/hashicorp/vsphere"
+      source  = "hashicorp/vsphere"
       version = "2.0.2"
     }
   }
