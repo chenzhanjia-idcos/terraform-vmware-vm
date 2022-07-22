dd78fd77f177c6cd52a322e2b2c470cfe13fbe6e
shihuanpeng <shihuanpeng@idcos.com>
2022-07-22 11:00:16 +0800

    vmware-module v1.0.0

--- provider.tf
+++ provider.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    ansible = {
-      source = "nbering/ansible"
-      version = "1.0.4"
-    }
     vsphere = {
       source  = "hashicorp/vsphere"
       version = "2.0.2"
